896e4bcf0e6547dff61849b0e7ca675ead7188a5
Cody Durr <cdurr@mit.edu>
2019-11-19 00:49:43 -0500

    Added support for more than 100 points sent from the UI

--- road-safety-api/api.py
+++ road-safety-api/api.py
@@ -20,14 +20,58 @@ def generate_path_request(points):
     
     return path_string
         
+def extract_roads(roads_api_response):
+    place_ids = set([])
+    for point in roads_api_response:
+        current_placeId = point['placeId']
+        if current_placeId not in place_ids:
+            place_ids.add(current_placeId)
+    roads = set([])
+    for placeId in place_ids:
+        place_query = "https://maps.googleapis.com/maps/api/place/details/json?place_id="+placeId+"&fields=name,rating,formatted_phone_number&key="+api_key
+        place = requests.get(place_query).content
+        place_json = json.loads(place)
+        # print(place_json)
+        try:
+            road_name = place_json['result']['name']
+        except:
+            pass
+
+        if road_name not in roads:
+            roads.add(road_name)
+    return roads
+
+#returns a 2D list of points
+def process_points(points):
+    final_points_arr = []
+    for i in range(0, (len(points) // 100) + 1, 100):
+        final_points_arr.append(points[i:min(len(points), i + 100)])
+    
+    # print(final_points_arr)
+    return final_points_arr
+
+def fetch_roads(subdivided_points):
+    all_points = []
+    for point_arr in subdivided_points:
+        path_string = generate_path_request(point_arr)
+        response_roads = requests.get('https://roads.googleapis.com/v1/snapToRoads?path='+path_string+'&interpolate=true&key='+api_key).content
+        locations = json.loads(response_roads)['snappedPoints']
+        all_points.extend(locations)
+
+    print(all_points)
+    return all_points
 
 @app.route('/analyze-route', methods=['GET', 'POST'])
 def analyze_route():
     data = request.json
     points =  data['points']
-    path_string = generate_path_request(points)
-    response_roads = requests.get('https://roads.googleapis.com/v1/snapToRoads?path='+path_string+'&interpolate=true&key='+api_key).content
-    return response_roads
+
+    subdivided_points = process_points(points)
+    response_roads = fetch_roads(subdivided_points)
+    roads = extract_roads(response_roads)
+
+    response = {"snappedPoints": response_roads}
+    return response
 
 if __name__ == "__main__":
     app.run()
